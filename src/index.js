662ce6b2386606c5a8d72dd2e5f688c5c68eb4af
Max16 <max16@naver.com>
2021-05-06 08:39:52 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count; // update 함수가 있어야 화면에 변화된 값이 적용
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  count++;
-  updateText();
-};
-const handleMinus = () => {
-  count--;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const countStore = createStore(countModifier);
