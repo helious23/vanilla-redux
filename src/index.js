49ef89f86baac6291179c0dc565cfbbf758d0fd3
Max16 <max16@naver.com>
2021-05-06 09:32:53 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
 
 number.innerText = 0;
@@ -23,11 +28,11 @@ const onChange = () => {
 countStore.subscribe(onChange); // subscribe : store 에 변화가 있을 때 onChange 호출
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" }); // dispatch : communicate with the store
+  countStore.dispatch({ type: ADD }); // dispatch : call the reducer(countModifier) and run action to current state, Must have "type" in "object"
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
